adff8c9edf144c55fdd904d36ca1bc8b3cf24424
DuranVasconcelos <74561215+DuranVasconcelos@users.noreply.github.com>
2020-11-25 17:08:00 -0300

    Delete fCn_rV.m

--- fCn_rV.m
+++
@@ -1,7 +0,0 @@
-function [a,c,CL,v] = fCn_rV (a,c,CL,v)
-%Função que estima a contribuição do estabilizador vertical para a derivada
-%Cn_r
-
-v.Cn_r = 2/a.b^2 *(v.l*cos(c.alpha)+ v.zmgc*sin(c.alpha))^2 *v.Cy_B;
-
-end
